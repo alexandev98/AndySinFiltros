87fb7e432f48fd59a04ffb7576c703070cead333
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-12-21 13:22:43 -0500

    Configuro el frontend bajo el patron MVC

---
+++ frontend/views/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Andy Sin Filtros</title>
+</head>
+<body>
+    <h1>AndySinFiltros</h1>
+    
+</body>
+</html>
