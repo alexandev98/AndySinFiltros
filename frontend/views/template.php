36acd7e42dba121fea7ec9d6de99461ffa217194
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-12-21 14:41:56 -0500

    Maqueto el cabezote en HTML5

--- frontend/views/template.php
+++ frontend/views/template.php
@@ -2,12 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="title" content="Andy Sin Filtros">
+    <meta name="description" content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aspernatur impedit obcaecati quaer">
+    <meta name="keywords" content="Andy, BLW">
     <title>Andy Sin Filtros</title>
+
+    <link rel="stylesheet" href="views/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="views/css/plugins/font-awesome.min.css">
+
+    <script src="js/plugins/jquery.min.cs"></script>
+    <script src="js/plugins/bootstrap.min.cs"></script>
+
 </head>
 <body>
-    <h1>AndySinFiltros</h1>
-    
+
+<?php
+
+// HEADER
+
+include "modules/header.php";
+
+?>
+
+
+
 </body>
 </html>
