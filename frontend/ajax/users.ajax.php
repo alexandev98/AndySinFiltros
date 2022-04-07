13da1f79fcddabc79f53e87663adf7505d7471f9
Marcelo Carvajal <marcelo_alec@hotmail.com>
2022-04-06 21:24:21 -0500

    Registro de usuarios con facebook - parte

--- frontend/ajax/users.ajax.php
+++ frontend/ajax/users.ajax.php
@@ -18,7 +18,27 @@ class AjaxUsers{
 
     }
 
-    
+    //FACEBOOK REGISTER
+
+    public $email;
+    public $name;
+    public $photo;
+
+    public function ajaxRegisterFacebook(){
+
+        $data = array("name"=>$this->name,
+                       "email"=>$this->email,
+                       "photo"=>$this->photo,
+                       "password"=>"null",
+                       "mode"=>"facebook",
+                       "verification"=>0,
+                       "emailCrypt"=>"null");
+
+        $response =  ControllerUsers::registerSocialMedia($data);
+
+        echo $response;
+    }
+
 
 }
 
@@ -29,3 +49,15 @@ if(isset($_POST["validateEmail"])){
     $valEmail->ajaxValidarEmail();
     
 }
+
+if(isset($_POST["email"])){
+    
+    $regFacebook = new AjaxUsers();
+    $regFacebook->email = $_POST["email"];
+    $regFacebook->name = $_POST["name"];
+    $regFacebook->photo = $_POST["photo"];
+    $regFacebook->ajaxRegisterFacebook();
+    
+}
+
+
