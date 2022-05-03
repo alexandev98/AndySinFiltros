20dbe5e2442757339698aa71f8e7b21b1e3f36c6
Marcelo Carvajal <marcelo_alec@hotmail.com>
2022-05-03 12:46:51 -0500

    Incorporo las etiquetas html para la pantalla de login

---
+++ backend/views/modules/login.php
@@ -0,0 +1,33 @@
+<div class="login-box">
+  <div class="login-logo">
+  <img src="views/img/template/logo.jpeg" class="img-responsive">
+  </div>
+  
+  <div class="login-box-body">
+
+    <p class="login-box-msg">Ingresar al sistema</p>
+
+    <form method="post">
+
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email" name="ingEmail">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" name="ingPassword">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+        </div>
+
+      </div>
+
+    </form>
+     
+  </div>
+  
+</div>
