bdcb3da3b0f11ad8f3a3ff61edfcbd94e94488eb
Marcelo Carvajal <marcelo_alec@hotmail.com>
2022-05-07 16:08:47 -0500

    Organizo el cabezote

--- backend/views/modules/header.php
+++ backend/views/modules/header.php
@@ -1,1 +1,47 @@
-<h1>Cabezote</h1>
+<header class="main-header">
+
+    <!-- logo -->
+    <a href="#" class="logo">
+      
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><img src="views/img/template/icono.png" class="img-responsive" style="padding:10px; filter:contrast(200%);"></span>
+      <!-- logo for regular state and mobile devices -->    
+      <span class="logo-lg"><img src="views/img/template/logo.jpeg" class="img-responsive" style="padding:10px 30px; filter:contrast(200%);"></span>
+    
+    </a>
+    <!-- logo -->
+
+    <!-- navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+		
+		 <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+		<!-- navbar-custom-menu -->
+    	<div class="navbar-custom-menu"> 
+
+    		<ul class="nav navbar-nav">
+			
+				<?php
+
+					include "header/notifications.php";
+
+					include "header/user.php";
+
+				?>
+
+    		</ul>
+
+    	</div>
+		<!-- navbar-custom-menu -->
+
+    </nav>
+    <!-- navbar -->
+
+
+</header>
---
+++ backend/views/modules/header/user.php
@@ -0,0 +1,47 @@
+
+<!-- user-menu -->
+<li class="dropdown user user-menu">
+
+	<!-- dropdown-toggle -->
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+	
+		<img src="views/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+		
+		<span class="hidden-xs">Tutoriales a tu Alcance</span>
+	
+	</a>
+	<!-- dropdown-toggle -->
+
+	<!-- dropdown-menu -->
+	<ul class="dropdown-menu">
+
+		<li class="user-header">
+		
+			<img src="views/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+			<p>
+			Tutoriales a tu Alcance
+			</p>
+		
+		</li>
+
+		<li class="user-footer">
+		
+			<div class="pull-left">
+				
+				<a href="perfil" class="btn btn-default btn-flat">Perfil</a>
+			
+			</div>
+			
+			<div class="pull-right">
+			
+				<a href="salir" class="btn btn-default btn-flat">Salir</a>
+			
+			</div>
+		</li>
+
+	</ul>
+	<!-- dropdown-menu -->
+
+</li>
+<!-- user-menu -->
